b72431c92c93edbdebe49cfb3969f62f3c04557d
gordonlin2011 <gordonlin2011@gmail.com>
2022-02-19 12:25:13 -0500

    Adding Lofi Hip Hop Radio
    Pro Program Lofi Hip Hop Radio added to main page

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 let button = document.getElementById("button");
-button.addEventListener
+button.addEventListener("click", handleclick);
+let lightswitch = false;
+
+
+
+
+
+
+
+
+
+function handleclick(){
+    console.log("Clicked!");
+    lightswitch = !lightswitch;
+    if(lightswitch == true){
+        let root = document.querySelector(":root");
+        root.style.setProperty("--stupidbgcolor", "#00BFB3");
+        root.style.setProperty("--stupid-text-color", "#023436");
+    }
+    else{
+        let root = document.querySelector(":root");
+        root.style.setProperty("--stupidbgcolor", "#023436");
+        root.style.setProperty("--stupid-text-color", "#00BFB3");
+    }
+}
